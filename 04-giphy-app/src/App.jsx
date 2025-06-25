d0dce9276e02a851082b49ffada9de3fac609245
Fannya G <steffanyscode30@gmail.com>
2025-06-24 18:46:01 -0600

    ✨  Show Trending Gifs

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -3,26 +3,26 @@ import ImageCard from './components/imageCard'
 import './App.css'
 
 function App() {
-    const [gifs, setGifs] = useState([])
-    const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+  const [gifs, setGifs] = useState([])
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY
 
-    useEffect(() => {
-        const fetchGifs = async ()  => {
-            try {
-                const response = await fetch(
-                    `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=20&offset=0&rating=g&bundle=messaging_non_clips`
-                )
-                const results = await response.json()
-                setGifs(results.data)
-            } catch (error) {
-                console.error('Error fetching GIFs:', error)
-            }
-        }
-        fetchGifs()
-    }, [])
+  useEffect(() => {
+    const fetchGifs = async () => {
+      try {
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=20&offset=0&rating=g&bundle=messaging_non_clips`
+        )
+        const results = await response.json()
+        setGifs(results.data)
+      } catch (error) {
+        console.error('Error fetching GIFs:', error)
+      }
+    }
+    fetchGifs()
+  }, [])
 
-    return (
-      <>
+  return (
+    <>
       <div className='App'>
         <div className='grid-cards'>
           {
